78e907ad4578e14ea436f3c37687b573846f0a37
emil19ro <emil19ro@gmail.com>
2023-07-28 08:02:34 +0200

    initial

---
+++ Javascript Weather app/scripts/forecast.js
@@ -0,0 +1,3 @@
+const key = 'PhBEiuhnXAM93241HlPHxT62tdQm4Xbi';
+const cityEndpoint = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+const conditionEndpoint = 'http://dataservice.accuweather.com/currentconditions/v1/{locationKey}';
